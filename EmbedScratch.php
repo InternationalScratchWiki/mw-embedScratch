aa9c4269dc8a6127fc4cf927db83fc432e8bae71
apple502j <33279053+apple502j@users.noreply.github.com>
2018-01-27 13:54:46 +0900

    v0.001

---
+++ EmbedScratch.php
@@ -0,0 +1,51 @@
+<?php
+/*
+    Copyright (C) 2018 Apple502j All rights reversed.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    
+    Embedding Scratch in MediaWiki
+    <scratch> Tag
+    
+    
+*/
+
+if (!defined('MEDIAWIKI')) {
+    die();
+}
+error_reporting(0);
+
+$wgExtensionFunctions[] = 'beginEmbedScratch';
+$wgHooks['ParserFirstCallInit'][] = 'parserEmbedScratch';
+
+function parserEmbedScratch (Parser $parser) {
+    $parser->setHook('scratch', 'sigRenderTag');
+    return true;
+}
+
+function parserEmbedScratch ($input, array $args, Parser $parser, PPFrame $frame) {
+    $project = $args["src"]?$args["src"]:"";
+    if ($url == ""){
+        return "";
+    }
+    $o =  '<br>'
+        . '<iframe allowtransparency="true" width="485" height="402" allowfullscreen frameborder="0" src="https://scratch.mit.edu/projects/embed/'
+        . $project
+        . '/?autostart=false"></iframe>';
+    return $o;
+}
+function beginEmbedScratch () {
+    global $wgOut;
+    $wgOut->addModules('ext.embedScratch');
+}
+
