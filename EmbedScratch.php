de02e64f4cea673b212544a92749d7987d410305
Ken <kenny2minecraft@gmail.com>
2019-05-23 19:03:02 +0800

    yobany urod

--- EmbedScratch.php
+++ EmbedScratch.php
@@ -64,7 +64,7 @@ class EmbedScratch{
 				"<div style=\"max-width:{$width}px\">"
 				. "<div style=\"position:relative;padding-top:"
 				. $height / $width * 100
-				. "px\">"
+				. "%\">"
 				. "<iframe "
 				. "allowtransparency=\"true\" "
 				. "width=\"100%\" height=\"100%\" "
